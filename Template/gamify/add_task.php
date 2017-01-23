d68cbc909091b528517059198c6750a6970711c9
robre <robert.reith@t-online.de>
2017-01-24 00:05:32 +0100

    no helper then..

--- Template/gamify/add_task.php
+++ Template/gamify/add_task.php
@@ -1,1 +1,11 @@
-<?= $this->myTask->selectExperience($values, $errors) ?>
+<?php 
+$attributes = array();
+$attributes = array_merge(array('tabindex="1000"'), $attributes);
+	$html = $this->helper->form->label(t('Experience for this Task'), 'experience');
+	$html .= $this->helper->form->numeric('experience', $values, $errors, $attributes);
+	$html .= ' '.t('EXP');
+        echo $html;
+
+
+?>
+<?= /**$this->myTask->selectExperience($values, $errors)**/ ?>
