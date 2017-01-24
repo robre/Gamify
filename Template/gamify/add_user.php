776e284084e98866aad04a2df874de1474bab2bb
robre <robert.reith@t-online.de>
2017-01-24 21:22:07 +0100

    asd

--- Template/gamify/add_user.php
+++ Template/gamify/add_user.php
@@ -1,4 +1,3 @@
 <li>
 	<?= $this->url->link('<i class="fa fa-diamond fa-fw"></i> My Experience', 'GamificationController', 'getuser', array('plugin' => 'gamify', 'user_id' => $this->user->getId())) ?>
-	   <!-- <?= /*$this->url->icon('diamond', t('My Experience'), 'GamificationController', 'getuser', array('plugin' => 'gamify', 'user_id' => $this->user->getId())) */?>-->
 </li>
