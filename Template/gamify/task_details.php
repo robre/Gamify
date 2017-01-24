f383ae6b4fcfacaafb0874e2a17ca3b3c8542727
robre <robert.reith@t-online.de>
2017-01-24 02:47:22 +0100

    asd

--- Template/gamify/task_details.php
+++ Template/gamify/task_details.php
@@ -1,3 +1,3 @@
 <li>
-                        <strong><?= t('Experience:') ?></strong> <span><?= $this->taskMetadataModel->get($task['id'], 'gamifyExperience', '10') ?></span>
+                        <strong><?= t('Experience:') ?></strong> <span><?= $this->model->taskMetadataModel->get($task['id'], 'gamifyExperience', '10') ?></span>
 </li>
